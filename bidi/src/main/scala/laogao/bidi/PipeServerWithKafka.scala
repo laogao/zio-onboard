eaea0baa7332e7db865207141ac9a3ce5f4342dd
laogao <gaoyuxiang@gmail.com>
2020-12-30 12:52:58 +0800

    logically split client stream in two

--- bidi/src/main/scala/laogao/bidi/PipeServerWithKafka.scala
+++ bidi/src/main/scala/laogao/bidi/PipeServerWithKafka.scala
@@ -1,16 +1,14 @@
 package laogao.bidi
 
 import io.grpc.Status
-import zio.{Schedule, ZEnv, ZIO}
+import zio.{Has, Schedule, ZEnv, ZIO, ZLayer, ZManaged}
 import laogao.bidi.bidi.ZioBidi.ZPipe
 import laogao.bidi.bidi.SmokeRequest
 import laogao.bidi.bidi.SmokeResponse
-import zio.stream.{Stream, ZStream}
+import zio.stream.{Stream, ZSink, ZStream}
 import scalapb.zio_grpc.ServerMain
 import scalapb.zio_grpc.ServiceList
 import zio.duration.durationInt
-import zio.ZLayer
-import zio.ZManaged
 import zio.blocking.Blocking
 import zio.clock.Clock
 import zio.kafka.consumer._
@@ -24,15 +22,15 @@ object PipeServerWithKafka extends ServerMain {
       val consumerSettings: ConsumerSettings = ConsumerSettings(List("10.0.0.8:9092")).withGroupId("group")
       val consumerManaged: ZManaged[Clock with Blocking, Throwable, Consumer.Service] = Consumer.make(consumerSettings)
       val consumer: ZLayer[Clock with Blocking, Throwable, Consumer] =  ZLayer.fromManaged(consumerManaged)
-      val kafkaStream: ZStream[ZEnv, Status, SmokeResponse] = Consumer.subscribeAnd(Subscription.topics("poc"))
+      val relay = request.filter("1" == _.message).flatMap(req =>
+        Consumer.subscribeAnd(Subscription.topics("poc"))
         .plainStream(Serde.string, Serde.string)
-        .provideSomeLayer[ZEnv](consumer)
+        .provideCustomLayer(consumer)
         .map(r => SmokeResponse(r.record.value))
-        .mapError(err => io.grpc.Status.UNKNOWN)
-      ZStream.mergeAll(2)(
-        request.flatMap(req => ZStream.fromEffect(ZIO.succeed(SmokeResponse(s"[ACK: ${req.message}]")))),
-        kafkaStream
-      )
+        .mapError(err => io.grpc.Status.UNKNOWN))
+      val ack = request.filter("2" == _.message).flatMap(req =>
+        ZStream.fromEffect(ZIO.succeed(SmokeResponse(s"[ACK: ${req.message}]"))))
+      ZStream.mergeAll(2)(relay, ack)
     }
   }
 
