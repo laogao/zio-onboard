2415ab7c886e7f12a9578063d9277e867a4a2afb
laogao <gaoyuxiang@gmail.com>
2021-01-13 20:41:07 +0800

    Typesafe config with fallback to reference.conf

--- etcd/src/main/scala/laogao/etcd/TypesafeConfigDemo.scala
+++ etcd/src/main/scala/laogao/etcd/TypesafeConfigDemo.scala
@@ -6,12 +6,13 @@ import zio.console.putStrLn
 
 object TypesafeConfigDemo extends zio.App {
 
+  val config = ConfigFactory.load()
+
   val program = for {
     _ <- putStrLn("First let's try Typesafe config to read environment variables.")
     _ <- putStrLn("Note that you have to place all config keys in application.conf, as such:")
     _ <- putStrLn("sys {\n path = ${?PATH}\n}")
-    _ <- putStrLn("Then you can val config = ConfigFactory.load and config.getString(\"sys.path\")")
-    config = ConfigFactory.load
+    _ <- putStrLn("Then you can val config = ConfigFactory.load() and config.getString(\"sys.path\")")
     _ <- putStrLn("You should now be able to get the $PATH:\n" + config.getString("sys.path"))
   } yield ()
 
